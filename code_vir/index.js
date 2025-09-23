f8b2b919455b3606aca2f8eeaac0a386e2999147
shivamdevraj02 <devrajshivam02@gmail.com>
2025-09-23 13:52:09 +0530

    Code_VIR

--- code_vir/index.js
+++ code_vir/index.js
@@ -17,8 +17,8 @@ const analyzeRouter = require('./router/analyzer');
 const progressRoute = require('./router/progress');
 const reminderRoute = require('./router/Reminder');
 
+const signRoute = require('./router/sign_up');
 
-// const CSERouter = require('./router/CSE');
 
 app.use(homeRoute);
 app.get("/test", (req, res)=>{
@@ -32,45 +32,12 @@ app.use(quizzesRouter);
 app.use(microRouter);
 app.use(flashcardsRouter);
 app.use(analyzeRouter);
-// app.use(CSERouter);
+app.use(signRoute);
 
-const PORT = 3000;
+const PORT = 8000;
 app.listen(PORT,()=>{
   console.log(`Server is started on address http://localhost:${PORT}`)
 })
 
 
 
-
-// const express = require('express');
-// const app = express();
-
-// // Use built-in body parser
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // Import routes
-// const homeRoute = require('./router/Home');
-// const coursesRouter = require('./router/courses');
-// // const quizzesRouter = require('./router/quizzes');
-// const loginRoute = require('./router/login');
-// const adminisRoute = require('./router/administration');
-// const CSERouter = require('./router/CSE');
-
-// // Use routes
-// // app.use(homeRoute);
-// // app.get("/test", (req, res) => {
-// //   res.send("Express is working!");
-// // });
-// app.use(coursesRouter);
-// // app.use(quizzesRouter);
-// app.use(loginRoute);
-// app.use(adminisRoute);
-// app.use(CSERouter);
-
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is started on address http://localhost:${PORT}`);
-// });
-
-// module.exports = app;
